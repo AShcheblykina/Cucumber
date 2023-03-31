e76a8caa0878adddef162b8dca3b4e9ab1cc72d9
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-03-31 17:40:01 +0300

    changed project

--- src/test/java/ru/netology/steps/TemplateSteps.java
+++ src/test/java/ru/netology/steps/TemplateSteps.java
@@ -12,24 +12,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TemplateSteps {
-   private DashboardPage dashboardPage;
+    private DashboardPage dashboardPage;
 
- @Пусть("Пусть пользователь залогинен с именем {} и паролем {}")
-  public void authorization(String login, String password) {
-  var loginPage= open("http://localhost:9999/", LoginPage.class);
-  var verificationPage = loginPage.validLogin(login,password);
-  dashboardPage = verificationPage.validVerify(DataHelper.getVerificationCode());
- }
- @Когда("Когда он переводит {} рублей с карты с номером {} на свою {} карту с главной страницы")
- public void makeTransfer(String amount, String debitCardNumber, int creditCardNumber) {
-  var transferPage = dashboardPage.selectCard(creditCardNumber);
-  dashboardPage = transferPage.makeTransfer(amount, debitCardNumber);
- }
- @Тогда("Тогда баланс его {} карты из списка на главной странице должен стать {} рублей")
- public void VerifyCreditBalance(int creditCardNumber, int expectedCreditBalance) {
- var actualCreditBalance = dashboardPage.getCardBalance(creditCardNumber);
- assertEquals(expectedCreditBalance,actualCreditBalance);
+    @Пусть("Пусть пользователь залогинен с именем {} и паролем {}")
+    public void authorization(String login, String password) {
+        var loginPage = open("http://localhost:9999/", LoginPage.class);
+        var verificationPage = loginPage.validLogin(login, password);
+        dashboardPage = verificationPage.validVerify(DataHelper.getVerificationCode());
+    }
 
- }
+    @Когда("Когда он переводит {} рублей с карты с номером {} на свою {} карту с главной страницы")
+    public void makeTransfer(String amount, String debitCardNumber, int creditCardNumber) {
+        var transferPage = dashboardPage.selectCard(creditCardNumber);
+        dashboardPage = transferPage.makeTransfer(amount, debitCardNumber);
+    }
+
+    @Тогда("Тогда баланс его {} карты из списка на главной странице должен стать {} рублей")
+    public void VerifyCreditBalance(int creditCardNumber, int expectedCreditBalance) {
+        var actualCreditBalance = dashboardPage.getCardBalance(creditCardNumber);
+        assertEquals(expectedCreditBalance, actualCreditBalance);
+
+    }
 
 }
